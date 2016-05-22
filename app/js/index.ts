7fbd5ab7618a0f88418bc6aa48cb1f35a59b4b67
wwju <wwju@yandex.ru>
2016-05-22 13:57:58 +0700

    fix configs - now typescript works

--- app/js/index.ts
+++ app/js/index.ts
@@ -1,41 +1,43 @@
 /// <reference path="../../typings/index.d.ts" />
 
+'use strict'
 
-var THREE = require('three')
-var a = require('./helper')
 
-var b = new a.default()
+var three = require('three')
+var a = require('./helper').default
+
+var b = new a()
 b.a()
 
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var scene = new three.Scene();
+var camera = new three.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new three.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var material = new THREE.MeshLambertMaterial({color: 0x55B663});
-var cube = new THREE.Mesh( geometry, material );
+var geometry = new three.BoxGeometry( 1, 1, 1 );
+// var material = new three.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new three.MeshLambertMaterial({color: 0x55B663});
+var cube = new three.Mesh( geometry, material );
 // scene.add( cube );
 
 camera.position.z = 20;
 
-var alight = new THREE.AmbientLight( 0x404040 ); // soft white light scene.add( light );
+var alight = new three.AmbientLight( 0x404040 ); // soft white light scene.add( light );
 scene.add(alight)
 
 
-var light = new THREE.PointLight(0xffffff);
+var light = new three.PointLight(0xffffff);
 light.position.set(60,10,10);
 scene.add(light);
 
-var geometry2 = new THREE.SphereGeometry( 1, 32, 32 );
-var material = new THREE.MeshLambertMaterial( {color: 0xffff00} );
+var geometry2 = new three.SphereGeometry( 1, 32, 32 );
+var material = new three.MeshLambertMaterial( {color: 0xffff00} );
 for (let i = 0; i<=2; i++) {
   console.log(i)
-  let sphere = new THREE.Mesh( geometry2, material );
+  let sphere = new three.Mesh( geometry2, material );
   sphere.position.x = 2
   sphere.position.y = i*5
   scene.add( sphere );
@@ -47,46 +49,46 @@ var keyA = false
 var keyW = false
 
 
-function onKeyUp(event) {
-  var keyCode = event.keyCode;
+// function onKeyUp(event) {
+//   var keyCode = event.keyCode;
 
-  switch (keyCode) {
-    case 68: //d
-      keyD = false;
-      break;
-    case 83: //s
-      keyS = false;
-      break;
-    case 65: //a
-      keyA = false;
-      break;
-    case 87: //w
-      keyW = false;
-      break;
-  }
-}
+//   switch (keyCode) {
+//     case 68: //d
+//       keyD = false;
+//       break;
+//     case 83: //s
+//       keyS = false;
+//       break;
+//     case 65: //a
+//       keyA = false;
+//       break;
+//     case 87: //w
+//       keyW = false;
+//       break;
+//   }
+// }
 
-function onKeyDown(event) {
-  var keyCode = event.keyCode;
+// function onKeyDown(event) {
+//   var keyCode = event.keyCode;
 
-  switch (keyCode) {
-    case 68: //d
-      keyD = true;
-      break;
-    case 83: //s
-      keyS = true;
-      break;
-    case 65: //a
-      keyA = true;
-      break;
-    case 87: //w
-      keyW = true;
-      break;
-  }
-}
+//   switch (keyCode) {
+//     case 68: //d
+//       keyD = true;
+//       break;
+//     case 83: //s
+//       keyS = true;
+//       break;
+//     case 65: //a
+//       keyA = true;
+//       break;
+//     case 87: //w
+//       keyW = true;
+//       break;
+//   }
+// }
 
-window.addEventListener("keyup", onKeyUp, false);
-window.addEventListener("keydown", onKeyDown, false);
+// window.addEventListener("keyup", onKeyUp, false);
+// window.addEventListener("keydown", onKeyDown, false);
 var coeff = 0.05
 var render = function () {
   requestAnimationFrame( render );
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var ts = require('gulp-typescript');
+var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 
 gulp.task('js:typescript', function () {
-	return gulp.src('app/js/index.ts')
-		.pipe(ts({
-			noImplicitAny: true,
-			out: 'output.js'
+	return gulp.src('app/js/*.ts')
+		.pipe(ts())
+		.pipe(gulp.dest('app/js'));
+});
+
+gulp.task('js:browserify', function () {
+  return gulp.src('app/js/index.js')
+		.pipe(browserify({
+		  insertGlobals : true,
+		  debug: true
 		}))
-		.pipe(rename('output.js'))
-		.pipe(gulp.dest('build/js'));
+    .pipe(rename('output_all.js'))
+		.pipe(gulp.dest('build/js'))
 });
 
 gulp.task('js:typescript:watch', function (cb) {
     gulp.watch('app/js/*.ts', ['js:typescript']);
 });
+
+gulp.task('js:js:watch', function (cb) {
+    gulp.watch('build/js/output.js', ['js:browserify']);
+});
+
+gulp.task('js:watch', ['js:typescript:watch', 'js:js:watch'])
