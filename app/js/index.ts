c36bd35a97cc45fdac683b765f2a1c42d42f59af
wwju <wwju@yandex.ru>
2016-05-22 14:00:49 +0700

    replace three -> THREE for typings autocomplete

--- app/js/index.ts
+++ app/js/index.ts
@@ -3,41 +3,40 @@
 'use strict'
 
 
-var three = require('three')
+var THREE = require('three')
 var a = require('./helper').default
 
 var b = new a()
 b.a()
 
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var scene = new three.Scene();
-var camera = new three.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-var renderer = new three.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new three.BoxGeometry( 1, 1, 1 );
-// var material = new three.MeshBasicMaterial( { color: 0x00ff00 } );
-var material = new three.MeshLambertMaterial({color: 0x55B663});
-var cube = new three.Mesh( geometry, material );
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshLambertMaterial({color: 0x55B663});
+var cube = new THREE.Mesh( geometry, material );
 // scene.add( cube );
 
 camera.position.z = 20;
 
-var alight = new three.AmbientLight( 0x404040 ); // soft white light scene.add( light );
+var alight = new THREE.AmbientLight( 0x404040 ); // soft white light scene.add( light );
 scene.add(alight)
 
 
-var light = new three.PointLight(0xffffff);
+var light = new THREE.PointLight(0xffffff);
 light.position.set(60,10,10);
 scene.add(light);
 
-var geometry2 = new three.SphereGeometry( 1, 32, 32 );
-var material = new three.MeshLambertMaterial( {color: 0xffff00} );
+var geometry2 = new THREE.SphereGeometry( 1, 32, 32 );
+var material = new THREE.MeshLambertMaterial( {color: 0xffff00} );
 for (let i = 0; i<=2; i++) {
   console.log(i)
-  let sphere = new three.Mesh( geometry2, material );
+  let sphere = new THREE.Mesh( geometry2, material );
   sphere.position.x = 2
   sphere.position.y = i*5
   scene.add( sphere );
