114681bd96ba6c5f37cb0aea626cfdcdba13cf3f
wwju <wwju@yandex.ru>
2016-05-25 09:18:55 +0700

    fix refactoring almoust amend

--- app/js/star.ts
+++ app/js/star.ts
@@ -9,25 +9,5 @@ var initialState = [{
 }]
 
 export var init = function(state) {
-    var stars = state.stars = initialState
-    var scene = state.scene.obj
-    stars.forEach(function(star: any) {
-        let material = new THREE.MeshPhongMaterial({
-            color: star.color,
-            shininess: 120,
-            emissive: 0xbb8383,
-            transparent: true,
-            opacity: 0.5
-        });
-
-        let geometry2 = new THREE.SphereGeometry(star.r, 32, 32);
-        let sphere = new THREE.Mesh(geometry2, material);
-        sphere.position.x = star.x
-        sphere.position.y = star.y
-        scene.add(sphere);
-
-        var light = new THREE.PointLight();
-        light.position.set(star.x, star.y, 0);
-        scene.add(light);
-    })
+    state.stars = initialState
 }
