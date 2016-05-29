c5edcc19f6e6d2b9b5999ad6f2c300dc893e14cc
wwju <wwju@yandex.ru>
2016-05-29 18:12:20 +0700

    add sun using particle engine (maybe, I'll replace it with a beautiful mesh)

--- app/js/star_view.ts
+++ app/js/star_view.ts
@@ -1,35 +1,76 @@
 /// <reference path="../../typings/index.d.ts" />
 import * as THREE from 'three'
+import SPE from './spe'
 
-var views = {}
-
-var createNew = function(index, star, scene) {
-    let material = new THREE.MeshPhongMaterial({
-        color: star.color,
-        shininess: 120,
-        emissive: 0xbb8383,
-        transparent: false,
-        opacity: 1
-    });
-    
-
-    let geometry2 = new THREE.SphereGeometry(star.r, 32, 32);
-    let sphere = new THREE.Mesh(geometry2, material);
-    sphere.position.x = star.x
-    sphere.position.y = star.y
-    scene.add(sphere);
-    
-    views[index] = sphere
-
-    var light = new THREE.PointLight(0xffffff, 1, 30);
-    light.position.set(star.x, star.y, 5.1);
-    scene.add(light);
+const smokeParticleUrl = 'app/smokeparticle.png'
+const speGroupProperties = {
+    texture: {
+        value: THREE.ImageUtils.loadTexture(smokeParticleUrl)
+    }
+}
+const emitterProperties = (position) => {
+  return {
+      maxAge: {
+            value: 5
+      },
+      position: {
+            value: position,
+            spread: new THREE.Vector3( 1, 1, 1 ),
+            distribution: SPE.distributions.SPHERE,
+            radius: 3,
+        },
+        color: {
+            value: [
+              new THREE.Color('white'),
+              new THREE.Color('red'),
+              new THREE.Color('yellow')
+            ]
+        },
+        size: {
+            value: 50
+        },
+        isStatic: false,
+        particleCount: 150
+    }
 }
 
-export default function(stars, scene) {
+var particleGroup, clock;
+var views = {}
+
+const addParticles = (scene, star) => {
+    var position = new THREE.Vector3(star.x, star.y, 0)
+    clock = new THREE.Clock()
+    particleGroup = new SPE.Group(speGroupProperties)
+    var emitter = new SPE.Emitter(emitterProperties(position))
+    particleGroup.addEmitter(emitter)
+    scene.add(particleGroup.mesh)
+}
+
+const update = () => {
+    particleGroup.tick();
+    particleGroup.tick(clock.getDelta());
+}
+
+const addLight = (scene: THREE.Scene, star) => {
+    var light = new THREE.PointLight(0xffffff, 1, 50);
+    light.position.set(star.x, star.y, star.z)
+    scene.add(light)
+}
+
+
+const createNew = (index, star, scene) => {
+    views[index] = true
+
+    addParticles(scene, star)
+    addLight(scene, star)
+}
+
+export default function(stars, scene, delta) {
     stars.forEach(function(star, index) {
         if (!views[index]) {
             createNew(index, star, scene)
+        } else {
+            update()
         }
     })
 }
