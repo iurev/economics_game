515762cbfe093b0ad284cc0fb18dc02c0fbe107c
wwju <wwju@yandex.ru>
2016-06-17 14:15:45 +0700

    replace inArray method with lodash method includes

--- app/js/camera.ts
+++ app/js/camera.ts
@@ -1,3 +1,5 @@
+import { includes } from 'lodash'
+
 const coeff = 1
 const minZ = 40
 
@@ -10,21 +12,17 @@ const initialState: Camera = {
   z: 200
 }
 
-const inArray = (array, elem) => {
-  return array.indexOf(elem) !== -1
-};
-
 export const updateValues = (camera, keys) => {
   let z = camera.z
-  if (inArray(keys, 76)) z += coeff
-  if (inArray(keys, 80)) z -= coeff
+  if (includes(keys, 76)) z += coeff
+  if (includes(keys, 80)) z -= coeff
   if (z < minZ) z = minZ
   camera.z = z
   let horCoeff = coeff / (250 / z)
-  if (inArray(keys, 68)) camera.x += horCoeff
-  if (inArray(keys, 83)) camera.y -= horCoeff
-  if (inArray(keys, 65)) camera.x -= horCoeff
-  if (inArray(keys, 87)) camera.y += horCoeff
+  if (includes(keys, 68)) camera.x += horCoeff
+  if (includes(keys, 83)) camera.y -= horCoeff
+  if (includes(keys, 65)) camera.x -= horCoeff
+  if (includes(keys, 87)) camera.y += horCoeff
 }
 
 export const init = (state) => {
